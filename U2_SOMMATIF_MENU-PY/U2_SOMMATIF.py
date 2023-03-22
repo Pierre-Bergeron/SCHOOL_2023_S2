ab366bb28692d14d6f621aba6f9911f3b266f2da
Pierre Bergeron <bergeronp881@icloud.com>
2023-03-22 18:00:44 -0400

    Added a simple (WAIT) function to see data before menu comes back.

--- U2_SOMMATIF_MENU-PY/U2_SOMMATIF.py
+++ U2_SOMMATIF_MENU-PY/U2_SOMMATIF.py
@@ -4,13 +4,15 @@
 # GIT    : SCHOOL_2023_S2
 
 import numpy as np
-import math
 
-# Legende (Ref d'article, quantite en stock, prix, commende automatique, description)
-ArrInv = np.array([[1, 10, 50.00, "Oui","USB power bank"],[2, 6, 15.99, "Oui", "Phone Charger"],[3, 2, 9.99, "Non", "Power Block USB"],[4, 26, 4.99, "Non", "USB Light"]])
+# Légende (Ref d'article, quantité en stock, prix, commende automatique, description)
+ArrInv = np.array([[1, 10, 50.00, "Oui", "USB power bank"],
+                   [2, 6, 15.99, "Oui", "Phone Charger"],
+                   [3, 2, 9.99, "Non", "Power Block USB"],
+                   [4, 26, 4.99, "Non", "USB Light"]])
 strSelection = 0
 while not (strSelection == "Q" or strSelection == "q"):
-  print("""
+    print("""
   Menu
   ----
   1 - Rechèrche d'article
@@ -20,54 +22,64 @@ while not (strSelection == "Q" or strSelection == "q"):
   5 - Suprimer un article
   6 - Commande Recu
   Q - Quitter
-
   """)
-  strSelection = input("Indiquer votre choix : ")
+    strSelection = input("Indiquer votre choix : ")
 
-  if strSelection == "1":
-    ArtId = int(input("Referance de l'article : "))
-    Id = ArtId - 1
-    Desc = ArrInv[Id][4]
-    Cost = ArrInv[Id][2]
-    Qty = ArrInv[Id][1]
-    IPFR_S = ArrInv[Id][3]
-    print("Article : ", Desc)
-    print("Prix : ", Cost, "$")
-    print("En Stock : ", Qty, " unité")
-    print("Orde Auto : ", IPFR_S)
+    if strSelection == "1":
+        ArtId = int(input("Referance de l'article : "))
+        Id = ArtId - 1
+        Desc = ArrInv[Id][4]
+        Cost = ArrInv[Id][2]
+        Qty = ArrInv[Id][1]
+        IPFR_S = ArrInv[Id][3]
+        print("Article : ", Desc)
+        print("Prix : ", Cost, "$")
+        print("En Stock : ", Qty, " unité")
+        print("Orde Auto : ", IPFR_S)
 
-  elif strSelection == "2":
-    ArtId = int(input("Referance de l'article : "))
-    Id = ArtId - 1
-    print("Quantité Disponible : ", ArrInv[Id][1])
-    AchQ = int(input("Quantité acheté : "))
-    prix = float(ArrInv[Id][2])
+        done = input("Retour? (Y) : ")
 
-    itc = ArrInv[Id][2]
-    sub = prix * AchQ
-    hst = round(sub * 0.13, 2)
-    tot = round(sub + hst, 2)
+    elif strSelection == "2":
+        ArtId = int(input("Referance de l'article : "))
+        Id = ArtId - 1
+        print("Quantité Disponible : ", ArrInv[Id][1])
+        AchQ = int(input("Quantité acheté : "))
+        prix = float(ArrInv[Id][2])
 
-    print("Sous-total -- ", sub, "$", " -- (", AchQ, " * ", itc, "$)")
-    print("HST (ON) ---- ", hst, "$")
-    print("Total ------- ", tot, "$")
-    conf = input("Voulez vous passé la transaction (O/N)? ")
-    if conf == "O":
-      ArrInv[Id][1] = float(ArrInv[Id][1]) - AchQ
-    else:
-      print("**Transaction Annuler**")
-    print("Nouvelle Quantité en Stock : ", ArrInv[Id][1])
+        itc = ArrInv[Id][2]
+        sub = prix * AchQ
+        hst = round(sub * 0.13, 2)
+        tot = round(sub + hst, 2)
 
-  elif strSelection == "3":
-    ArtId = int(input("Referance de l'article : "))
-    Id = ArtId - 1
-    print("Quantité En Stock : ", ArrInv[Id][1])
-    NewQ = int(input("Quantité Actuelle : "))
-    ArrInv[Id][1] = NewQ
-    print("Quantité changer à ", ArrInv[Id][1], " en stock.")
+        print("Sous-total -- ", sub, "$", " -- (", AchQ, " * ", itc, "$)")
+        print("HST (ON) ---- ", hst, "$")
+        print("Total ------- ", tot, "$")
+        conf = input("Voulez vous passé la transaction (O/N)? ")
+        if conf == "O":
+            ArrInv[Id][1] = float(ArrInv[Id][1]) - AchQ
+        else:
+            print("**Transaction Annuler**")
+        print("Nouvelle Quantité en Stock : ", ArrInv[Id][1])
 
-  elif strSelection == "4":
-    print("4")
+        done = input("Retour? (Y) : ")
 
+    elif strSelection == "3":
+        ArtId = int(input("Referance de l'article : "))
+        Id = ArtId - 1
+        print("Quantité En Stock : ", ArrInv[Id][1])
+        NewQ = int(input("Quantité Actuelle : "))
+        ArrInv[Id][1] = NewQ
+        print("Quantité changer à ", ArrInv[Id][1], " en stock.")
 
+        done = input("Retour? (Y) : ")
+
+    elif strSelection == "4":
+        print("4")
+
+    elif strSelection == "5":
+        print("5")
+
+    elif strSelection == "6":
+        print("6")
+print("Passé une belle journée.")
 # Les autres options suivent.
