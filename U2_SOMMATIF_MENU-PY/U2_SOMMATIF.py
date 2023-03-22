90deb8def0e0f5bfe7da4d0f82b76e9cc6b0fdd4
Pierre Bergeron <bergeronp881@icloud.com>
2023-03-22 15:32:47 -0400

    BASE CODE NEEDED

--- U2_SOMMATIF_MENU-PY/U2_SOMMATIF.py
+++ U2_SOMMATIF_MENU-PY/U2_SOMMATIF.py
@@ -1,1 +1,28 @@
 #start
+# Exemple d'un système de menu de base.
+import numpy as np
+
+tblInv = np.array()
+strSelection = 0
+while not (strSelection == "Q" or strSelection == "q"):
+  print("""
+  Menu
+  ----
+  1 - Saisir les valeurs
+  2 - Effacer les valeurs
+  3 - Imprimer les valeurs à l'écran
+  4 - 
+  5 - 
+  6 - 
+  Q - Quitter
+
+  """)
+  strSelection = input("Indiquer votre choix : ")
+
+  if strSelection == "1":
+    print("Ici, le programme exécute l'option 1.")
+  elif strSelection == "3":
+    print("Ici, le programme exécute l'option 1.")
+  elif strSelection == "3":
+    print("Ici, le programme exécute l'option 3.")
+  # Les autres options suivent.
