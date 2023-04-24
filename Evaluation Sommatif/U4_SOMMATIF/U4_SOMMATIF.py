369e35a79870dd27919fc0af6b25da04437c288f
Pierre Bergeron <bergeronp881@icloud.com>
2023-04-24 13:58:16 -0400

    Creation de la class() Refrigo

--- Evaluation Sommatif/U4_SOMMATIF/U4_SOMMATIF.py
+++ Evaluation Sommatif/U4_SOMMATIF/U4_SOMMATIF.py
@@ -2,4 +2,29 @@
 # Date   :
 # Titre  : U4_SOMMATIF
 # GIT    : SCHOOL_2023_S2
-# DESCR. :
+# DESCR. : Calcule et compare le volume/dimantion de (3) differante electromeneager
+
+import math
+
+
+class Refrigo:
+    def __init__(self):
+        self.intObjLargeur = 0
+        self.intObjHauteur = 0
+        self.intObjProfondeur = 0
+
+        self.intEmpLargeur = 0
+        self.intEmpHauteur = 0
+        self.intEmpProfondeur = 0
+
+    def check(self):
+        if self.intObjLargeur <= self.intEmpLargeur:
+            if self.intObjHauteur <= self.intEmpHauteur:
+                if self.intObjProfondeur <= self.intEmpProfondeur:
+                    print("Le {} va faire".format(self.__init__()))
+
+
+objFridge = Refrigo()
+objFridge
+
+
