68baca866b23015afcda1b79623b1330817775e0
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-11 11:54:34 -0400

    add function 2,3

--- Evaluation Sommatif/U4_SOMMATIF/U4_SOMMATIF.py
+++ Evaluation Sommatif/U4_SOMMATIF/U4_SOMMATIF.py
@@ -9,19 +9,39 @@ import math
 
 class Refrigo:
     def __init__(self):
-        self.intObjLongueur = 0
-        self.intObjHauteur = 0
-        self.intObjProfondeur = 0
-
+        # espace limit de lobjet
         self.intEmpLongueur = 82
         self.intEmpHauteur = 170
         self.intEmpProfondeur = 75
 
+        # option 1 (dimention exterieur, exterieur)
+        self.intObjLongueur = 0
+        self.intObjHauteur = 0
+        self.intObjProfondeur = 0
+
         self.inIntObjLongueur = 0
         self.inIntObjHauteur = 0
         self.inIntObjProfondeur = 0
+        # option 2
+        self.intObjLongueur2 = 0
+        self.intObjHauteur2 = 0
+        self.intObjProfondeur2 = 0
+
+        self.inIntObjLongueur2 = 0
+        self.inIntObjHauteur2 = 0
+        self.inIntObjProfondeur2 = 0
+        # option 3
+        self.intObjLongueur3 = 0
+        self.intObjHauteur3 = 0
+        self.intObjProfondeur3 = 0
+
+        self.inIntObjLongueur3 = 0
+        self.inIntObjHauteur3 = 0
+        self.inIntObjProfondeur3 = 0
 
         self.fit_isTrue = 0
+        self.fit_isTrue2 = 0
+        self.fit_isTrue3 = 0
 
     def check(self):
         if self.intObjLongueur <= self.intEmpLongueur:
@@ -36,9 +56,49 @@ class Refrigo:
         else:
             print("Ne fait pas")
 
+    def check2(self):
+        if self.intObjLongueur2 <= self.intEmpLongueur:
+            if self.intObjHauteur2 <= self.intEmpHauteur:
+                if self.intObjProfondeur2 <= self.intEmpProfondeur:
+                    self.fit_isTrue2 = 1
+                    print("Le réfigérateur va faire")
+                else:
+                    print("Ne fait pas")
+            else:
+                print("Ne fait pas")
+        else:
+            print("Ne fait pas")
+
+    def check3(self):
+        if self.intObjLongueur3 <= self.intEmpLongueur:
+            if self.intObjHauteur3 <= self.intEmpHauteur:
+                if self.intObjProfondeur3 <= self.intEmpProfondeur:
+                    self.fit_isTrue3 = 1
+                    print("Le réfigérateur va faire")
+                else:
+                    print("Ne fait pas")
+            else:
+                print("Ne fait pas")
+        else:
+            print("Ne fait pas")
+
     def cal_vol(self):
         if self.fit_isTrue == 1:
             volume = self.inIntObjLongueur * self.inIntObjHauteur * self.inIntObjProfondeur
+            print("Le volume du réfrigérateur est: {} cm³".format(volume))
+        else:
+            print("Le volume na pas été calculer, tandis que le réfrigérateur ne fait pas.")
+
+    def cal_vol2(self):
+        if self.fit_isTrue2 == 1:
+            volume = self.inIntObjLongueur2 * self.inIntObjHauteur2 * self.inIntObjProfondeur2
+            print("Le volume du réfrigérateur est: {} cm³".format(volume))
+        else:
+            print("Le volume na pas été calculer, tandis que le réfrigérateur ne fait pas.")
+
+    def cal_vol3(self):
+        if self.fit_isTrue3 == 1:
+            volume = self.inIntObjLongueur3 * self.inIntObjHauteur3 * self.inIntObjProfondeur3
             print("Le volume du réfrigérateur est: {} cm³".format(volume))
         else:
             print("Le volume na pas été calculer, tandis que le réfrigérateur ne fait pas.")
@@ -64,6 +124,10 @@ Refrigo.inIntObjHauteur = int(input("Hauteur du réfrigérateur: "))
 Refrigo.inIntObjProfondeur = int(input("Profondeur du réfrigérateur: "))
 
 objFridge.check()
+objFridge.check2()
+objFridge.check3()
 objFridge.cal_vol()
+objFridge.cal_vol2()
+objFridge.cal_vol3()
 
 
