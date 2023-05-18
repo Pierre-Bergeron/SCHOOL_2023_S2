a7a3c17ec7bfa42fa81c78f29e954dafce4f7b8a
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-18 13:54:08 -0400

    Menu Basic Code

--- Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
+++ Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
@@ -1,1 +1,38 @@
 # Main document
+
+
+def find(id_ref):
+
+def pull(id_ref, id_sel):
+
+def push(id_ref, id_sel, data_push):
+
+
+
+while not (strSelection == "Q" or strSelection == "q"):
+    print("""
+  Menu
+  ----
+  1 - 
+  2 - 
+  3 - 
+  4 -  
+  5 - 
+  6 - 
+  Q - Quitter
+  """)
+    strSelection = input("Indiquer votre choix : ")
+
+    if strSelection == "1":
+
+    elif strSelection == "2":
+        print()
+    elif strSelection == "3":
+        print()
+    elif strSelection == "4":
+        print()
+    elif strSelection == "5":
+        print()
+    elif strSelection == "6":
+        print()
+print("Passé une belle journée.")
