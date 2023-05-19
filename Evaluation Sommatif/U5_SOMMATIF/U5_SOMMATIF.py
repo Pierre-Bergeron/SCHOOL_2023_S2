6ed92bdc2bd32e57ee4c07112bbeec2efded9cd1
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-19 14:05:17 -0400

    Class Fixed to pull employee names

--- Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
+++ Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
@@ -4,8 +4,8 @@
 
 
 class Employee_List(object):
-    def __init__(self, employee):
-        self._employee = [None]
+    def __init__(self, employee_id):
+        self._employee = [None] * employee_id
 
     def __setitem__(self, employee_number, data):
         self._employee[employee_number] = data
@@ -15,16 +15,16 @@ class Employee_List(object):
 
 
 employees_all = Employee_List(10)  # Create 10 employee
-employees_all[0] = 'Pierre Bergeron'
-employees_all[1] = 'Simon Dubosq'
-employees_all[2] = 'Regis Godin'
-employees_all[3] = 'Samuel Baril'
-employees_all[4] = 'Olivier Girouard'
-employees_all[5] = 'Nicola Girouard'
-employees_all[6] = 'FirstN LastN'
-employees_all[7] = 'Hello World'
-employees_all[8] = 'Pablo Megan'
-employees_all[9] = 'Soupe Friday'
+employees_all[0] = 'Pierre.Bergeron'
+employees_all[1] = 'Simon.Dubosq'
+employees_all[2] = 'Regis.Godin'
+employees_all[3] = 'Samuel.Baril'
+employees_all[4] = 'Olivier.Girouard'
+employees_all[5] = 'Nicola.Girouard'
+employees_all[6] = 'FirstN.LastN'
+employees_all[7] = 'Hello.World'
+employees_all[8] = 'Pablo.Megan'
+employees_all[9] = 'Soupe.Friday'
 print(employees_all[2])
 
 strSelection = ''
