406b704bc95182f1596f2c86e573cf2f0492ea1d
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-19 14:01:05 -0400

    Added class

--- Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
+++ Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
@@ -1,40 +1,31 @@
 # Main document
 
 # Class for employee details
-class Employee_Information:
 
-    def __init__(self, name, dob, empId):
-        self.name = name
-        self.dob = dob
-        self.empId = empId
 
 class Employee_List(object):
     def __init__(self, employee):
         self._employee = [None]
+
     def __setitem__(self, employee_number, data):
         self._employee[employee_number] = data
+
     def __getitem__(self, employee_number):
         return self._employee[employee_number]
 
-    employees_all = Employee_List(10)  # Construct a building with 4 floors
-    employees_all[0] = 'Pierre Bergeron'
-    employees_all[1] = 'Simon Dubosq'
-    employees_all[2] = 'Regis Godin'
-    employees_all[3] = 'Samuel Baril'
-    employees_all[4] = 'Olivier Girouard'
-    employees_all[5] = 'Nicola Girouard'
-    employees_all[6] = 'FirstN LastN'
-    employees_all[7] = 'Hello World'
-    employees_all[8] = 'Pablo Megan'
-    employees_all[9] = 'Soupe Friday'
-    print(employees_all[2])
 
-
-def find(id_ref):
-
-def pull(id_ref, id_sel):
-
-def push(id_ref, id_sel, data_push):
+employees_all = Employee_List(10)  # Create 10 employee
+employees_all[0] = 'Pierre Bergeron'
+employees_all[1] = 'Simon Dubosq'
+employees_all[2] = 'Regis Godin'
+employees_all[3] = 'Samuel Baril'
+employees_all[4] = 'Olivier Girouard'
+employees_all[5] = 'Nicola Girouard'
+employees_all[6] = 'FirstN LastN'
+employees_all[7] = 'Hello World'
+employees_all[8] = 'Pablo Megan'
+employees_all[9] = 'Soupe Friday'
+print(employees_all[2])
 
 strSelection = ''
 while not (strSelection == "Q" or strSelection == "q"):
@@ -52,7 +43,7 @@ while not (strSelection == "Q" or strSelection == "q"):
     strSelection = input("Indiquer votre choix : ")
 
     if strSelection == "1":
-
+        print()
     elif strSelection == "2":
         print()
     elif strSelection == "3":
