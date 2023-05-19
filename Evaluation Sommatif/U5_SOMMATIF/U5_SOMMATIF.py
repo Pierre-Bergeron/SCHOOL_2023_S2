1af8d469965a2fb1b906fd1a6d6c9fec6991e819
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-19 16:00:49 -0400

    created set function

--- Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
+++ Evaluation Sommatif/U5_SOMMATIF/U5_SOMMATIF.py
@@ -7,27 +7,54 @@ class Employee_List(object):
     def __init__(self, employee_id):
         self._employee = [None] * employee_id
 
-    def __setitem__(self, employee_number, data):
-        self._employee[employee_number] = data
+    def __setitem__(self, employee_number, name):
+        self._employee[employee_number] = name
 
     def __getitem__(self, employee_number):
         return self._employee[employee_number]
 
 
+
 employees_all = Employee_List(10)  # Create 10 employee
-employees_all[0] = 'Pierre Bergeron'
-employees_all[1] = 'Simon Dubosq'
-employees_all[2] = 'Regis Godin'
-employees_all[3] = 'Samuel Baril'
-employees_all[4] = 'Olivier Girouard'
-employees_all[5] = 'Nicola Girouard'
-employees_all[6] = 'FirstN LastN'
-employees_all[7] = 'Hello World'
-employees_all[8] = 'Pablo Megan'
-employees_all[9] = 'Soupe Friday'
+employees_all[0] = 'Pierre Bergeron', '12121.00', '000-000-000'
+employees_all[1] = 'Simon Dubosq', '10000.00', '000-000-000'
+employees_all[2] = 'Regis Godin', '10000.00', '000-000-000'
+employees_all[3] = 'Samuel Baril', '10000.00', '000-000-000'
+employees_all[4] = 'Olivier Girouard', '10000.00', '000-000-000'
+employees_all[5] = 'Nicola Girouard', '10000.00', '000-000-000'
+employees_all[6] = 'FirstN LastN', '10000.00', '000-000-000'
+employees_all[7] = 'Hello World', '10000.00', '000-000-000'
+employees_all[8] = 'Pablo Megan', '10000.00', '000-000-000'
+employees_all[9] = 'Soupe Friday', '10000.00', '000-000-000'
+
+def set_emp_salerie(id, salerie):
+    name = employees_all[id][0]
+    employees_all[id] = name, salerie
+
+def set_emp_social(id, ssn):
+    name = employees_all[id][0]
+    salerie = employees_all[id][1]
+    employees_all[id] = name, salerie, ssn
+
+def set_emp_name(id, name):
+    salerie = employees_all[id][1]
+    ssn = employees_all[id][2]
+    employees_all[id] = name, salerie, ssn
 
 
-print(employees_all[3])
+
+
+print(employees_all[0])
+
+# verification
+set_emp_salerie(0, 10000)
+print(employees_all[0][1])
+# verification
+set_emp_social(0, "100-200-300")
+print(employees_all[0][2])
+# verification
+set_emp_name(0, "Pierre Bergy")
+print(employees_all[0][0])
 
 # def add_emp(name, dob, salerie, ssn):
 
