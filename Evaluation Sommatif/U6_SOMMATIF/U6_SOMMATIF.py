eb5d259246894fe8d7d7055687588fad9d8d7a9f
Pierre Bergeron <bergeronp881@icloud.com>
2023-05-23 15:16:48 -0400

    page 2 article id search added basic

--- Evaluation Sommatif/U6_SOMMATIF/U6_SOMMATIF.py
+++ Evaluation Sommatif/U6_SOMMATIF/U6_SOMMATIF.py
@@ -3,9 +3,42 @@
 # File : U6_SOMMATIF
 
 import tkinter as tk
+import numpy as np
 import math
 import datetime
-import random
+
+
+# Article Array
+ArrInv = np.array([[1, 10.0, 50.00, "Actif", "USB power bank"],
+                   [2, 6.0, 15.99, "Actif", "Phone Charger"],
+                   [3, 2.0, 9.99, "Null", "Power Block USB"],
+                   [4, 26.0, 4.99, "Null", "USB Light"],
+                   [7, 87.0, 38.77, "Actif", "Wireless Charger"],
+                   [6, 21.0, 21.99, "Null", "White Brd Case (30pc)"],
+                   [5, 0.0, 19.98, "Null", "Christmas Lights"]])
+
+# Global function
+# id_sel (1 = Desc, 2 = IPFR status, 3 = inventory, 4 = prix, 5 = ref)
+def pull(id_ref, id_sel):
+    scr_id = id_ref - 1
+    if id_sel == 1:
+        desc = ArrInv[scr_id][4]
+        return desc
+    elif id_sel == 2:
+        ipfr = ArrInv[scr_id][3]
+        return ipfr
+    elif id_sel == 3:
+        qty_inv = ArrInv[scr_id][1]
+        return qty_inv
+    elif id_sel == 4:
+        prix = ArrInv[scr_id][2]
+        return prix
+    elif id_sel == 5:
+        ref_id_arr = ArrInv[scr_id][0]
+        return ref_id_arr
+    else:
+        null = "null"
+        return null
 
 # Main window settings
 pageA = tk.Tk()
@@ -20,35 +53,62 @@ pageA.columnconfigure(0, weight=1)
 date = datetime.date.today()
 datenow = "Date :", date, "|"
 
-
-
-
-
-
-
-
-
 # Frame settings
 def show_frame(frame):
     frame.tkraise()
 
+def find_ref(id_ref):
+    row = 0
+    arraylen = len(ArrInv)
+    while row < arraylen:
+        if id_ref == ArrInv[row][0]:
+            rowId = row + 1
+            return rowId
+        else:
+            row = row + 1
 # element for de l'exam et choix du frame
 page1 = tk.Frame(pageA)
-WinNum = tk.Frame(pageA)
-for frame in (page1, WinNum):
+page2 = tk.Frame(pageA)
+page3 = tk.Frame(pageA)
+page4 = tk.Frame(pageA)
+page5 = tk.Frame(pageA)
+page6 = tk.Frame(pageA)
+page7 = tk.Frame(pageA)
+
+
+for frame in (page1, page2, page3, page4, page5, page6, page7):
     frame.grid(row=0, column=0, sticky='nsew')
 
 page1['bg'] = '#becee5'
-WinNum['bg'] = '#E5F6DF'
+page2['bg'] = '#E5F6DF'
+page3['bg'] = '#E5F6DF'
+page4['bg'] = '#E5F6DF'
+page5['bg'] = '#E5F6DF'
+page6['bg'] = '#E5F6DF'
+page7['bg'] = '#E5F6DF'
 
-# FRAME 1 - lotterieChoise ( Choisir vos numbero)
+# Frame Window Change Btn
+btn_frame_page2 = tk.Button(page1, text="Page 2", highlightbackground='#becee5', command=lambda: show_frame(page2))
+btn_frame_page2.grid(row=5, column=5, columnspan='2')
+btn_frame_page3 = tk.Button(page1, text="Page 3", highlightbackground='#becee5', command=lambda: show_frame(page3))
+btn_frame_page3.grid(row=6, column=5, columnspan='2')
+btn_frame_page4 = tk.Button(page1, text="Page 4", highlightbackground='#becee5', command=lambda: show_frame(page4))
+btn_frame_page4.grid(row=7, column=5, columnspan='2')
+btn_frame_page5 = tk.Button(page1, text="Page 5", highlightbackground='#becee5', command=lambda: show_frame(page5))
+btn_frame_page5.grid(row=8, column=5, columnspan='2')
+btn_frame_page6 = tk.Button(page1, text="Page 6", highlightbackground='#becee5', command=lambda: show_frame(page6))
+btn_frame_page6.grid(row=9, column=5, columnspan='2')
+btn_frame_page7 = tk.Button(page1, text="Page 7", highlightbackground='#becee5', command=lambda: show_frame(page7))
+btn_frame_page7.grid(row=10, column=5, columnspan='2')
+
+# frame 1 (page1) - option selection screen ( choisir son option)
 SpaceUs = tk.Label(page1, text='                               ')
 SpaceUs.grid(row=0)
 
-page1Titre = tk.Label(page1, text='page 1 :', bg="#D3D3D3")
+page1Titre = tk.Label(page1, text='Menu', bg="#D3D3D3")
 page1Titre.grid(row='0', column='0',  sticky='n')
 
-page2Titre = tk.Label(page1, text='page2 :', bg="#becee5")
+page2Titre = tk.Label(page1, text="Recherche d'article", bg="#becee5")
 page2Titre.grid(row='1', column='0', sticky='e')
 
 UF_CH1 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
@@ -67,110 +127,63 @@ UF_CH5 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
 UF_CH5.grid(row=1, column=5)
 UF_CH5.insert(0, "0")
 UF_CH6 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
-UF_CH6.grid(row=1, column=6)
-UF_CH6.insert(0, "0")
-UF_CH7 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
-UF_CH7.grid(row=1, column=7)
-UF_CH7.insert(0, "0")
-UF_CH8 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
-UF_CH8.grid(row=1, column=8)
-UF_CH8.insert(0, "0")
-UF_CH9 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
-UF_CH9.grid(row=1, column=9)
-UF_CH9.insert(0, "0")
-UF_CH10 = tk.Entry(page1, bg="#FFFFFF", width='5', highlightbackground='#becee5')
-UF_CH10.grid(row=1, column=10)
-UF_CH10.insert(0, "0")
 
 
-
-UF_CH_SAVE = tk.Button(page1, text="Acheter", highlightbackground='#becee5', command=None)
-UF_CH_SAVE.grid(row=2, column=5, columnspan='2')
-
-
-
-# Bouton pour traiter les numero
-VERNUM = tk.Label(page1, text='Vos numéro :', bg='#becee5')
-VERNUM.grid(row=3, column=0, sticky='e')
+# UF_CH_SAVE = tk.Button(page1, text="Acheter", highlightbackground='#becee5', command=None)
+# UF_CH_SAVE.grid(row=2, column=5, columnspan='2')
 
 lotterieChoiseDate = tk.Label(page1, text=datenow, bg='#becee5')
 lotterieChoiseDate.grid(row=2, column='0', sticky='e')
 
-UF_CH_SUB = tk.Button(page1, text="Joué Mètenet", highlightbackground='#becee5', command=lambda: show_frame(WinNum))
-UF_CH_SUB.grid(row=5, column=5, columnspan='2')
 
 # Demontre les numero que tu a choisi dans le 2e frame
-Titre2 = tk.Label(WinNum, text='Vos Numéro :', bg='#E5F6DF')
+def find_article_page2():
+    article_id_sumitted = find_ref(articleNum_Input.get())
+    article_desc = pull(article_id_sumitted, 1)
+    article_prix = pull(article_id_sumitted, 4)
+    article_ipfr = pull(article_id_sumitted, 2)
+    article_invt = pull(article_id_sumitted, 3)
+
+    desc_page2_txt['text'] = article_desc
+    prix_page2_txt['text'] = article_prix
+    ipfr_page2_txt['text'] = article_ipfr
+    invt_page2_txt['text'] = article_invt
+
+# frame 2 (page 2) search item by article
+Titre2 = tk.Label(page2, text="Numéro d'article :", bg='#E5F6DF')
 Titre2.grid(row=0, column=0, sticky='e')
-NUM1 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM1.grid(row=0, column=1)
-NUM2 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM2.grid(row=0, column=2)
-NUM3 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM3.grid(row=0, column=3)
-NUM4 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM4.grid(row=0, column=4)
-NUM5 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM5.grid(row=0, column=5)
-NUM6 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM6.grid(row=0, column=6)
-NUM7 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM7.grid(row=0, column=7)
-NUM8 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM8.grid(row=0, column=8)
-NUM9 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM9.grid(row=0, column=9)
-NUM10 = tk.Label(WinNum, text='', bg='#E5F6DF')
-NUM10.grid(row=0, column=10)
 
-# Demontre les numero gangnent dans le 2e frame
-WinN = tk.Label(WinNum, text='Numéro Gagnant :', bg='#E5F6DF')
-WinN.grid(row=1, column=0, sticky='e')
-WinN1 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN1.grid(row=1, column=1)
-WinN2 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN2.grid(row=1, column=2)
-WinN3 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN3.grid(row=1, column=3)
-WinN4 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN4.grid(row=1, column=4)
-WinN5 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN5.grid(row=1, column=5)
-WinN6 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN6.grid(row=1, column=6)
-WinN7 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN7.grid(row=1, column=7)
-WinN8 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN8.grid(row=1, column=8)
-WinN9 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN9.grid(row=1, column=9)
-WinN10 = tk.Label(WinNum, text='', bg='#E5F6DF')
-WinN10.grid(row=1, column=10)
+articleNum_Input = tk.Entry(page2, bg="#FFFFFF", width=5, highlightbackground='#becee5')
+articleNum_Input.grid(row=0, column=1, columnspan=2)
 
-R1 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R1.grid(row=2, column=1)
-R2 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R2.grid(row=2, column=2)
-R3 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R3.grid(row=2, column=3)
-R4 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R4.grid(row=2, column=4)
-R5 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R5.grid(row=2, column=5)
-R6 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R6.grid(row=2, column=6)
-R7 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R7.grid(row=2, column=7)
-R8 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R8.grid(row=2, column=8)
-R9 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R9.grid(row=2, column=9)
-R10 = tk.Label(WinNum, text='', bg='#E5F6DF')
-R10.grid(row=2, column=10)
+articleNum_Submit = tk.Button(page2, text="Recherché l'article", highlightbackground='#becee5', command=find_article_page2)
+articleNum_Submit.grid(row=0, column=3, columnspan=2)
 
-Status = tk.Label(WinNum, text='État :', bg='#E5F6DF')
-Status.grid(row=3, column=0, sticky='e')
-Status1 = tk.Label(WinNum, text='', bg='#E5F6DF')
+desc_page2_lbl = tk.Label(page2, text='Description :', bg='#E5F6DF')
+desc_page2_lbl.grid(row=2, column=0, sticky='e')
+desc_page2_txt = tk.Label(page2, text='', bg='#E5F6DF')
+desc_page2_txt.grid(row=2, column=1)
+
+prix_page2_lbl = tk.Label(page2, text='Prix /Unité :', bg='#E5F6DF')
+prix_page2_lbl.grid(row=3, column=0, sticky='e')
+prix_page2_txt = tk.Label(page2, text='', bg='#E5F6DF')
+prix_page2_txt.grid(row=3, column=1)
+
+ipfr_page2_lbl = tk.Label(page2, text='IPFR Status :', bg='#E5F6DF')
+ipfr_page2_lbl.grid(row=4, column=0, sticky='e')
+ipfr_page2_txt = tk.Label(page2, text='', bg='#E5F6DF')
+ipfr_page2_txt.grid(row=4, column=1)
+
+invt_page2_lbl = tk.Label(page2, text='Inventaire  :', bg='#E5F6DF')
+invt_page2_lbl.grid(row=5, column=0, sticky='e')
+invt_page2_txt = tk.Label(page2, text='', bg='#E5F6DF')
+invt_page2_txt.grid(row=5, column=1)
+
+
+
+Status = tk.Label(page2, text='État :', bg='#E5F6DF')
+Status.grid(row=10, column=0, sticky='e')
+Status1 = tk.Label(page2, text='', bg='#E5F6DF')
 Status1.grid(row=3, column=1, columnspan=5)
 
 show_frame(page1)
